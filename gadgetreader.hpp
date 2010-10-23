6f0e754b4e7a3e82dfd018d544329b3443a04150
Simeon Bird <spb41@cam.ac.uk>
2010-10-23 15:21:24 +0100

    Make documentation better and use javadoc format

--- gadgetreader.hpp
+++ gadgetreader.hpp
@@ -42,11 +42,12 @@
 
 namespace GadgetReader{
 
-  //The Gadget file header.
-  /*This differs from the standard formulation by using explicit integer sizes. 
+  /** The Gadget file header.
+   * This differs from the standard formulation by using explicit integer sizes.
    * Note however that at least one implementation of N-GenICs does NOT have 
-   * the NallHW field, setting it to something arbitrary.*/
-  typedef struct {
+   * the NallHW field, setting it to something arbitrary.
+   */
+ typedef struct {
     uint32_t npart[N_TYPE];
     double   mass[N_TYPE];
     double   time;
@@ -67,22 +68,23 @@ namespace GadgetReader{
     char     fill[256- N_TYPE*sizeof(uint32_t)- (6+N_TYPE)*sizeof(double)- (7+2*N_TYPE)*sizeof(int32_t)];  /* fills to 256 Bytes */
   } gadget_header;
  
-  //Things that we don't want wrapped
+  // The following are private structures that we don't want wrapped
 #ifndef SWIG 
   //Filename type
   typedef std::string f_name;
   
-  //This is to hold information about a given block
-  typedef struct{
+  /** This private structure is to hold information about a given block. May change without warning, don't use it*/
+ typedef struct{
     //Starting position in the file
     int64_t start_pos;
     uint32_t length; //in bytes, excluding the two integer "record sizes" at either end
     short partlen; //length for a single particle. Likely to be 4 or 12.
   } block_info;
   
-  //This structure stores information about each file. 
-  //Stores block maps, headers, 
-  typedef struct{
+  /** This private structure stores information about each file. May change without warning, don't use it
+   * Stores block maps, caches headers
+   */
+ typedef struct{
     //The file's actual name
     f_name name;
     //The header is stored inline, as it is small
@@ -105,7 +107,7 @@ namespace GadgetReader{
                    * and the third is a list of block headers for format 1 files. 
                    * If not set, will use the default hard-coded values. */
                   GSnap(std::string snap_filename, bool debugf=true, std::vector<std::string> *BlockNames=NULL);
-                  /* Reads particles from a file block into the memory pointed to by block
+                  /** GetBlock: Reads particles from a file block into the memory pointed to by block
                    * Returns the number of particles read.
                    * Takes: block name, 
                    *        particles to read, 
@@ -116,80 +118,102 @@ namespace GadgetReader{
                    *        Only skip types for which the block is actually present:
                    *        Unfortunately there is no way of the library knowing
                    *        which particle has which type,
-                   *        so there is no way of telling that in advance.  */
-                  /*This function is also insanity with respect to bindings, for 
+                   *        so there is no way of telling that in advance.
+                   *This function is also insanity with respect to bindings, for
                    * reasons which probably have to do with the total lack of memory or type safety.
-                   * Therefore don't bind it and provide wrapper functions. */
+                   * Therefore we don't bind it and provide wrapper functions. */
                 #ifndef SWIG
                   int64_t GetBlock(std::string BlockName, void *block, int64_t npart_toread, int64_t start_part, int skip_type);
                 #endif
-                  /* Memory-safe wrapper functions for the bindings. It is not anticipated that people writing codes in C 
+                  /** GetBlock overload returning a vector
+                   * Memory-safe wrapper functions for the bindings. It is not anticipated that people writing codes in C
                    * will want to use these, as they need to allocate a significant quantity of temporary memory.
                    * We do not attempt to work out whether the block requested is a float or an int.*/
                   std::vector<float> GetBlock(std::string BlockName, int64_t npart_toread, int64_t start_part, int skip_type);
-                  /* Support getting IDs: is exactly the same as the above*/
+                  /** GetBlock overload returning an int
+                   * This supports getting IDs: is exactly the same as the above*/
                   std::vector<int> GetBlockInt(std::string BlockName, int64_t npart_toread, int64_t start_part, int skip_type);
                   /* Ideally here we would have a wrapper for returning 3-float blocks such as POS and VEL, 
                    * BUT SWIG can't handle nested classes, so we can't do that.*/
 
-                  //Tests whether a particular block exists
+                  /** IsBlock:
+                   * Tests whether a particular block exists
+                   */
                   bool IsBlock(std::string BlockName);
-                  //Gets a file header
-                  //Note this means GetHeader().Npart[0] != GetNpart(0)
-                  //This has to be the case to avoid overflow issues.
+                  /** GetHeader:
+                   * Gets a file header
+                   * Note this means GetHeader().Npart[0] != GetNpart(0)
+                   * This has to be the case to avoid overflow issues. */
                   gadget_header GetHeader(int i=0);
-                  //Get the filename we put in
+                  /** GetFileName:
+                   * Return the filename of the snapshot, the original argument of the constructor*/
                   std::string GetFileName(){
                           return base_filename;
                   }
-                  //Get the number of files we found in the snapshot
+                  /** GetNumFiles:
+                   * Get the number of files we actually found in the snapshot */
                   int GetNumFiles(){
                           return file_maps.size();
                   }
-                  //Get the file format: first bit is format 2, second is swap_endian
+                  /** GetFormat:
+                   * Get the file format: first bit is format 2, second is swap_endian
+                   * Allows us to test if we have Gadget 1 or endian swapped files */
                   int GetFormat(){
                           return swap_endian*2+(!format_2);
                   }
-                  /*Convenience function to get the total number of particles easily for a type
+                  /** GetNpart:
+                   * Convenience function to get the total number of particles easily for a type
                    * Note when calculating total header, to add npart, not to use npart total, 
                    * in case we have more than 2**32 particles.
                    * First argument is particle type, second, if true will return the 
                    * particles actually found instead of those the snapshot is reporting to exist*/
                   int64_t GetNpart(int type, bool found=false);
-                  //Get total size of a block in the snapshot, in bytes.
+                  /** GetBlockSize:
+                   * Get total size of a block in the snapshot, in bytes.
+                   * Useful for allocating memory*/
                   int64_t GetBlockSize(std::string BlockName);
-                  //Get number of particles a block has data for
+                  /** GetBlockParts:
+                   * Get number of particles a block has data for. Same as above but divided by partlen */
                   int64_t GetBlockParts(std::string BlockName);
-                  //Get a list of all blocks present in the snapshot.
+                  /** GetBlocks:
+                   * Get a list of all blocks present in the snapshot, as a set */
                   std::set<std::string> GetBlocks();
-                  /*Set the per-particle length for a given block to partlen.
+                  /** SetPartLen:
+                   * Set the per-particle length for a given block to partlen.
                    * This could be useful if the automatic detection failed.*/
                   void SetPartLen(std::string BlockName, short partlen);
-                  /*These functions have no purpose except to help the bindings*/
-/*                  int64_t GetBlock(std::string BlockName, float *block, int64_t npart_toread, int64_t start_part, int skip_type){
-                          return GetBlock(BlockName, block, npart_toread/sizeof(float),start_part,skip_type);
-                  }*/
           private:
-                  //Function to check the headers of different files are 
-                  //consistent with each other.
+                  /** check_headers: Private function to check whether two headers are
+                   * consistent with being from the same snapshot*/
                   DLL_LOCAL bool check_headers(gadget_header head1, gadget_header head2);
-                  //Construct a map of where the blocks start and finish for a single file
+                  /** construct_file_map: Private function that does the hard work of looking over a file
+                   * and constructing a map of where the blocks start and finish*/
                   DLL_LOCAL file_map construct_file_map(FILE *file,f_name filename, std::vector<std::string> *BlockNames);
-                  /*Sets swap_endian and format_2. 
+                  /** check_filetype: Private function to detect endian swapping or format 1 files
+                   * Sets swap_endian and format_2.
                    * Returns 0 for success, 1 for an empty file, and 2 
                    * if the filetype is weird (eg, if you tried to open a text file by mistake)*/
                   DLL_LOCAL int check_filetype(FILE* fd);
-                  /*Read the block header*/
+                  /** read_G2_block_head: Private function to read the block (not the file) header */
                   DLL_LOCAL uint32_t read_G2_block_head(char* name, FILE *fd, const char * file);
   
-                  //Base filename for the snapshot
+                  /** Base filename for the snapshot*/
                   f_name base_filename;
-                  bool swap_endian; //Do we want to swap the enddianness of the files?
-                  bool format_2; //Are we using format 2 files?
-                  bool bad_head64; //This flag indicates that the header is setting 
-                                  //the 64 bit part of nparttotal incorrectly
-                  std::vector<file_map> file_maps; //Pointer to the file data
-                  /*Does WARN print anything?*/
+
+                  /** Stores whether the file is endian swapped*/
+                  bool swap_endian;
+
+                  /** Stores whether we are using format 1 or 2 files*/
+                  bool format_2;
+
+                  /** This flag is a silly hack to indicate whether the header is setting
+                   * the long word part of nparttotal incorrectly, as some versions of Genics do*/
+                  bool bad_head64;
+
+                  /** Vector to store the maps of each simulation snapshot*/
+                  std::vector<file_map> file_maps;
+
+                  /** Flag to control whether WARN prints anything */
                   bool debug;
   };
 
